5cbe9dcd4372b9caae11a4eaa573662d09bfc7f7
Jack Ridley <ridley.jj@outlook.com>
2025-10-07 11:09:14 +0100

    Created Initial Test Strings

---
+++ JrString.js
@@ -0,0 +1,95 @@
+class JrString {
+    stringStart;
+    stringEnd;
+    pullPoint;
+    isMouseAbove;
+
+    sinOsc = 0; // for oscillating the string
+    midPoint;
+    maxAmplitude;
+    currentAmplitude = 0;
+    damping = 0.97;
+    minMouseSpeed = 1;
+    maxMouseSpeed = 60;
+
+    _absIncrement = 0.1;
+    increment = this._absIncrement;
+
+    isOscillating = false;
+
+    constructor(startPoint, length, thickness = 3) {
+        this.thickness = thickness;
+        this.stringStart = startPoint.copy();
+        this.stringEnd = createVector(startPoint.x + length, startPoint.y);
+        this.midPoint = createVector(
+            this.stringStart.x + (this.stringEnd.x - this.stringStart.x) * 0.5,
+            this.stringStart.y + (this.stringEnd.y - this.stringStart.y) * 0.5
+        );
+        this.pullPoint = this.midPoint.copy();
+
+        this.maxAmplitude = length * 0.25;
+
+        this.isMouseAbove = this.isPointAboveString(mouseX, mouseY);
+    }
+
+    triggerDelay = 100; // ms
+    lastTriggerTime = 0;
+
+    triggerString(intensity = 1, direction) {
+        if (this.isOscillating && millis() - this.lastTriggerTime < this.triggerDelay) return;
+
+        this.increment = this._absIncrement * (direction ? -1 : 1);
+        this.currentAmplitude = this.maxAmplitude * intensity;
+        this.sinOsc = 0;
+        this.isOscillating = true;
+        this.lastTriggerTime = millis();
+    }
+
+    draw() {
+
+        this.calculatePullPoint();
+
+        this.checkForStringTrigger();
+
+        noFill();
+        strokeWeight(this.thickness);
+
+        beginShape();
+        vertex(this.stringStart.x, this.stringStart.y);
+        let test = bezierVertex(this.pullPoint.x, this.pullPoint.y, this.pullPoint.x, this.pullPoint.y, this.stringEnd.x, this.stringEnd.y);
+        endShape();
+    }
+
+    calculatePullPoint() {
+        this.sinOsc += this.increment;
+        this.pullPoint.y = this.midPoint.y + sin(this.sinOsc) * this.currentAmplitude;
+        this.currentAmplitude *= this.damping;
+        if (this.currentAmplitude != 0 && this.currentAmplitude < 1) {
+            this.currentAmplitude = 0;
+            this.isOscillating = false;
+        }
+    }
+
+    isPointAboveString(x, y) {
+        return y < (this.pullPoint.y - this.midPoint.y) * sin((x - this.stringStart.x) * PI / (this.stringEnd.x - this.stringStart.x)) + this.midPoint.y;
+    }
+
+    checkForStringTrigger() {
+        if (mouseX < this.stringStart.x || mouseX > this.stringEnd.x)
+            return; // outside string horizontal bounds
+
+        let mouseSpeedY = constrain(abs(mouseY - pmouseY), this.minMouseSpeed, this.maxMouseSpeed);
+        mouseSpeedY -= this.minMouseSpeed;
+        mouseSpeedY /= this.maxMouseSpeed; // normalise
+
+        if (mouseSpeedY == 0)
+            return; // not moving vertically
+
+        let currentlyAboveString = this.isPointAboveString(mouseX, mouseY);
+        let previouslyAboveString = this.isPointAboveString(pmouseX, pmouseY);
+        if (currentlyAboveString != previouslyAboveString) {
+            this.triggerString(mouseSpeedY, currentlyAboveString);
+        }
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+var oscStrings = [];
+
+function setup() {
+  createCanvas(windowWidth, 600);
+  
+  for (let i = 0; i < 6; i++) {
+    oscStrings.push(new JrString(createVector(0, height * 0.4 + i * height  * 0.05), width, 3));
+  }
+}
+
+function draw() {
+  background(255);
+
+  for (let i = 0; i < oscStrings.length; i++) {
+    oscStrings[i].draw();
+  }
+}
